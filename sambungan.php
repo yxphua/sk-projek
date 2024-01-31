b84993692534eb18a28d8fd172dc12779861d40b
yxphua <134066886+yxphua@users.noreply.github.com>
2024-01-31 22:00:59 +0800

    Create sambungan.php

---
+++ sambungan.php
@@ -0,0 +1,9 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "kehadiran";
+
+    $sambungan = mysqli_connect($host, $user, $password, $database)
+    or die("Sambugan gagal");
+?>
