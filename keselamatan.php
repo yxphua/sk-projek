8e489a260ec3af72c9c6effb0a0b931845067655
yxphua <134066886+yxphua@users.noreply.github.com>
2024-02-01 21:05:56 +0800

    Create keselamatan.php

---
+++ keselamatan.php
@@ -0,0 +1,10 @@
+<?php
+
+    if(session_status() == PHP_SESSION_NONE){
+        session_start();
+    }
+
+    //if (!isset($_SESSION["idpengguna])) {
+    //  echo "<script>window.location='logout.php'</script>";
+    // }
+?>
