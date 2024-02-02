b383d9398ef9313031b6d3f15e319ef2e725faab
yxphua <134066886+yxphua@users.noreply.github.com>
2024-02-02 23:33:23 +0800

    Create ahli_menu.php

---
+++ ahli_menu.php
@@ -0,0 +1,38 @@
+<?php 
+    include("keselamatan.php");  
+?>
+
+<link rel="stylesheet" href="aamenu.css">
+
+<header>
+    <img class="logo" src="imej/logo.jpg">
+    <img class="kelab" src="imej/namakelab.png">
+</header>
+
+<nav>
+    <ul>
+        <li>
+            <a href="ahli_home.php"><b>HOME</b></a>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <a href="ahli_daftar.php"><b>DAFTAR</b></a>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <a href="aktiviti_carian.php"><b>CARIAN</b></a>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <a href="ahli_laporan.php"><b>LAPORAN</b></a>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <a href="logout.php"><b>KELUAR</b></a>
+        </li>
+    </ul>
+</nav>
