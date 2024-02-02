3913822363f601ddc76f27df383840152a97e5a7
yxphua <134066886+yxphua@users.noreply.github.com>
2024-02-02 23:14:49 +0800

    Create admin_home.php

---
+++ admin_home.php
@@ -0,0 +1,19 @@
+<?php
+    include("keselamatan.php");
+    include("sambungan.php");
+    include("admin_menu.php");
+
+    echo "<main class='aktiviti'>";
+
+    $sql = "select * from aktiviti";
+    $result = mysqli_query($sambungan, $sql);
+    while($aktiviti = mysqli_fetch_array($result)) {
+        echo "<figure>
+                  <img class='home' src='imej/$aktiviti[gambar]'>
+                  <figcaption>$aktiviti[namaaktiviti]</figcaption>
+              </figure>";
+    }
+    echo "</main>";
+
+    include("footer.php");
+?>
